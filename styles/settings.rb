89a2354e3f193e4da917339b57240259b8431a28
Gant <GantMan@gmail.com>
2013-03-10 21:19:25 -0500

    Commented out but possibly useful gradient usage

--- styles/settings.rb
+++ styles/settings.rb
@@ -2,6 +2,14 @@ Teacup::Stylesheet.new :settings do
   import :base
 
   v_padding = 10
+  gradient_top_color = 0xc3a477.uicolor 
+  gradient_bottom_color = 0x9f8061.uicolor
+
+  # Commented out because it doesn't seem to play nicely with landscape
+  #style :settings,
+  #  gradient: { 
+  #    colors: [gradient_top_color.CGColor, gradient_bottom_color.CGColor] 
+  #  }
 
   style :label, extends: :custom_label,
     constraints: [
