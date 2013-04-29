36ee6fa0862d7ab8f805dcf8a31abc08434ccd28
Gant <GantMan@gmail.com>
2013-04-29 16:01:42 -0500

    Pushing the title text down correctly

--- lib/handlers.rb
+++ lib/handlers.rb
@@ -1,6 +1,8 @@
 Teacup.handler UIView, :nav_background do |view, hash|
   UINavigationBar.appearance.setBackgroundImage(hash[:normal].uiimage, forBarMetrics:UIBarMetricsDefault)
+  UINavigationBar.appearance.setTitleVerticalPositionAdjustment(4.0, forBarMetrics:UIBarMetricsDefault)
   UINavigationBar.appearance.setBackgroundImage(hash[:landscape].uiimage, forBarMetrics:UIBarMetricsLandscapePhone)
+  UINavigationBar.appearance.setTitleVerticalPositionAdjustment(7.0, forBarMetrics:UIBarMetricsLandscapePhone)
 end
 
 Teacup.handler UIView, :nav_btn_tint do |color|
